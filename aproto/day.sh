49b68ba22aa5a9674499235c7af5e26ec4aef54b
d08ble <d08ble@gmail.com>
2014-12-05 05:22:38 +0200

    data changed

--- aproto/day.sh
+++ aproto/day.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-day
+
+convert -font Ravie -pointsize 72  label:'1' -border 10 \
+          -tile tile_aqua.jpg   -draw "color 0,0 reset"  \
+          -tile tile_water.jpg -gravity center -annotate +0+0 'Get Wet!' \
+          autosize_wet.jpg
